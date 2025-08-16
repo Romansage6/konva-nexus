651f2a7bc5a488e71abb163b1fb016663bfcc973
Romansage6 <romansage39@gmail.com>
2025-08-16 22:55:43 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
-import MainApp from "@/components/MainApp";
+// ADD (imports):
+import AnimeTab from "./discovery/AnimeTab";
+import AnimeDetails from "./anime/AnimeDetails";
 
-const Index = () => {
-  return <MainApp />;
-};
-
-export default Index;
+// ADD (within your <Routes>):
+<Route path="/discovery/anime" element={<AnimeTab />} />
+<Route path="/anime/:id" element={<AnimeDetails />} />
