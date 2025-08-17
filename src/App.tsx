ea8bc0836d27f2fd3435d7a7f19a74b59cab3654
Romansage6 <romansage39@gmail.com>
2025-08-17 21:04:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import Discovery from "./pages/Discovery";
-import Profile from "./pages/Profile";
-import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
+// ✅ 1. Add these imports at the top
+import AnimeTab from "./pages/DiscoveryTabs/AnimeHub";
+import AnimeDetails from "./pages/DiscoveryTabs/AnimeDetails";
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/discovery" element={<Discovery />} />
-          <Route path="/profile" element={<Profile />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      {/* All your app routes go here */}
+      <Routes>
+        <Route path="/" element={<Discovery />} />
 
-export default App;
+        {/* ✅ 2. Add these routes inside <Routes> */}
+        <Route path="/discovery/anime" element={<AnimeTab />} />
+        <Route path="/anime/:id" element={<AnimeDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
